15574c6ebb3d11fe7ec72105e1ddd0b4e27f72b7
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-04 20:45:50 +0100

    comit

--- primer juego.py
+++ primer juego.py
@@ -2,18 +2,17 @@ import random
 numero = random.randint(0,100)
 print(numero)
 print("Intento es un numero entero")
-intento_1 = input()
+intento_1 = int(input())
 
-while str(intento_1) != str(numero):
+while intento_1 != numero:
     
-    if str(intento_1) > str(99):
+    if intento_1 > 99:
         print("Choose another number between 0 an 99")
     else:
-        if str(intento_1) > str(numero):
+        if intento_1 > numero:
             print("Te has pasado un poco. El numero esta entre:" + "0 y" + str(intento_1))
         else:
-        #if str(intento_1) < str(numero):
             print("Te has quedado corto. El numero esta entre:" + str(intento_1) + "y 99")
-    intento_1 = input()
-if str(intento_1) == str(numero):
+    intento_1 = int(input())
+if intento_1 == numero:
      print("¡Congratulations!")
